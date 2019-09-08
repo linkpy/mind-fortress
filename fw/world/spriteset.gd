46d869a1aaf61d7dc5ee05b8193014aec2766c64
Nils 'Linkie Pi' Reid <linkpydev@gmail.com>
2019-09-08 13:17:42 +0200

    Added named sprites in the SpriteSheet class.

--- fw/world/spriteset.gd
+++ fw/world/spriteset.gd
@@ -6,28 +6,52 @@ var texture: Texture
 var count: Vector2
 var size: Vector2
 
+var names = []
+
 
 
 func _init(p: String, c: Vector2, s: Vector2) -> void:
 	texture = load(p)
 	count = c
 	size = s
+	
+	names.resize(int(c.x*c.y))
 
 
 
 func get_sprite_position(id: int) -> Vector2:
+	assert(id >= 0 and id < int(count.x*count.y))
+	
 	return Vector2(
 		id % int(count.x),
 		int(id / count.x)
 	)
 
 func get_sprite_index(pos: Vector2) -> int:
+	assert(pos.x >= 0 and pos.x < count.x)
+	assert(pos.y >= 0 and pos.y < count.y)
+	
 	return int(pos.y * count.x + pos.x)
 
 func get_sprite_rectangle(id: int) -> Rect2:
+	assert(id >= 0 and id < int(count.x*count.y))
+	
 	return Rect2(
 		get_sprite_position(id) * size,
 		size
 	)
 
 
+
+func name_sprite(id: int, n: String):
+	assert(id >= 0 and id < int(count.x*count.y))
+	
+	names[id] = n
+	return self
+
+func import_names(sid: int, list: Array):
+	for i in range(list.size()):
+		name_sprite(sid+i, list[i])
+
+func get_named_sprite_index(n: String) -> int:
+	return names.find(n)
--- world/spriteset.gd
+++ world/spriteset.gd
@@ -7,7 +7,21 @@ const TEXTURE = "res://world/assets/tileset.png"
 const TILE_COUNT = Vector2(16,16)
 const TILE_SIZE = Vector2(18,18)
 
+const NAMES = [
+	" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4",
+	"5", "6", "7", "8", "9", ".", ",", ";", ":", "!", "?", "/", "+", "-", "*", "$",
+	"%", "^", ">", "<", "=", "_", "~", '"', "'", "#", "(", ")", "[", "]", "{", "}",
+	"2~", 
+	"full", 
+	"half",
+	"vwall", "hwall", "xwall",
+	"trwall", "tlwall", "tdwall", "tuwall",
+	"cdwall", "crwall", "cuwall", "clwall",
+	"fullwall",
+	"plot", "plant", "tree", "grass"
+]
 
 
 func _init().(TEXTURE, TILE_COUNT, TILE_SIZE):
-	pass
+	import_names(0, NAMES)
